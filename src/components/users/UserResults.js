50bdddff3b7d8a21d042ff4430c1890c8eea496b
raaghavnedrahm <raaghavendrahm@gmail.com>
2021-12-23 16:15:26 +0530

    UserItem comp is created and used in UserResutls. Next, states and functions in UserResults will be moved to the newly created context.

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
@@ -27,7 +28,7 @@ const UserResults = () => {
     return (
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
         {users.map((user) => (
-          <h3 key={user.id}>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
