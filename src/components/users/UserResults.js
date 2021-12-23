68c9ad79d2ec51e8ea32ca8a6ba6a83ae5ac7889
raaghavnedrahm <raaghavendrahm@gmail.com>
2021-12-23 17:42:31 +0530

    GithubContext is created. "users", "loading" states and "fetchUsers" function are moved to GithubContext. All the components in App.js are wrapped inside GithubProvider. In UserResults, useContext and GithubContext are used to access states and functions from GithubContext. This was done is previous method. Next, reducers and useReducer hook are used to improve this.

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,28 +1,16 @@
 import { useEffect } from 'react';
-import { useState } from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import { useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
 
 const UserResults = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // From context:
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
