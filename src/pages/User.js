0f8afa2192fa21115fa32117e52652985059b730
raaghavnedrahm <raaghavendrahm@gmail.com>
2021-12-29 17:50:43 +0530

    User is modified for useParams import. "build" is added to gitignore and made it untracked by git, a new PAT is generated.

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useContext } from 'react';
 import GithubContext from '../context/github/GithubContext';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Spinner from '../components/layout/Spinner';
